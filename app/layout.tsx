04c802e423aefffcb83ef235adb053433ccf192f
yamani59 <yamaniyud@gmail.com>
2023-04-08 22:38:15 +0700

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
+      </head>
       <body>{children}</body>
     </html>
   );

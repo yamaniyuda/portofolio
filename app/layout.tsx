aa2b374f0369a32e35ff6de70e9a28e7dc20c094
yamani59 <yamaniyud@gmail.com>
2023-04-08 16:32:45 +0700

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,15 @@ import "./globals.css";
 export const metadata = {
   title: "Yamani. Yuda",
   description: "My portofolio 2023",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: '/favicon.ico',
+    other: {
+      rel: '/favicon.ico',
+      url: '/favicon.ico',
+    },
+  },
 };
 
 export default function RootLayout({

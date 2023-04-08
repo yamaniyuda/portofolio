d05b72272ba23a2d4d603fef823b0b99ed49dcc3
yamani59 <yamaniyud@gmail.com>
2023-04-08 16:29:11 +0700

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Yamani. Yuda',
-  description: 'My portofolio 2023',
-}
+  title: "Yamani. Yuda",
+  description: "My portofolio 2023",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
